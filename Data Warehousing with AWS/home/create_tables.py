54fd93cbe643d4fa37ee9c02c33712d026baf1d4
alimayo <33448280+alimayo@users.noreply.github.com>
2021-04-21 16:50:48 +0500

    Update create_tables.py

--- Data Warehousing with AWS/home/create_tables.py
+++ Data Warehousing with AWS/home/create_tables.py
@@ -4,18 +4,28 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops the tables if already exists
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates the tables 
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Connects the program to postgres on the AWS Redshift cluster
+    Calls drop and create table functions
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
